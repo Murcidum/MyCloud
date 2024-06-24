2363768a5319a9b7d493da789a143a86e12cbed3
Murcidum <73168797+Murcidum@users.noreply.github.com>
2024-06-25 01:21:15 +0500

    Add files via upload

---
+++ myCloud/src/test/java/com/example/myCloud/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.myCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(HomeControllerTest.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get(""));
+    }
+}
